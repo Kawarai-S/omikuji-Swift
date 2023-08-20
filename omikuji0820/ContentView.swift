404c914627f3832e80bef19486dd760981ac63bb
Kawarai-S <kayawakakururu@icloud.com>
2023-08-20 14:11:13 +0900

    モディファイア作成

--- omikuji0820/ContentView.swift
+++ omikuji0820/ContentView.swift
@@ -10,10 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("omikuji")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            HStack{
+                Button("リセット"){
+                    
+                }
+                .modifier(ButtonModifier(backgroundColor: .red))
+                
+                Button("おみくじを引く"){
+                    
+                }
+                .modifier(ButtonModifier(backgroundColor: .green))
+                
+            }
         }
         .padding()
     }
@@ -22,5 +33,15 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ButtonModifier: ViewModifier{
+    let backgroundColor: Color
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, minHeight: 50)
+            .background(backgroundColor)
+            .foregroundColor(.white)
     }
 }
