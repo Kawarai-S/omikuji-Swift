6fe95edcadf29a60618644e3c6bce6743ea5f9a0
Kawarai-S <kayawakakururu@icloud.com>
2023-08-20 11:16:24 +0900

    Initial Commit

---
+++ omikuji0820/omikuji0820App.swift
@@ -0,0 +1,17 @@
+//
+//  omikuji0820App.swift
+//  omikuji0820
+//
+//  Created by 瓦井つばさ on 2023/08/20.
+//
+
+import SwiftUI
+
+@main
+struct omikuji0820App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
